de14a308945107fa7bde8277ecc3e24daf248d9a
Andrew Reid Marderstein <amarder@smsh11dsu-srcf-d15-36.scg.stanford.edu>
2022-04-21 07:43:55 -0700

    update

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -3,8 +3,10 @@
 # example run:
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=160G --nodes=1 --ntasks=1 --cpus-per-task=1 run_script.sh plots_v2.py
 
+echo "Start"
 source /oak/stanford/groups/smontgom/amarder/bin/conda_init.sh 
 conda activate minimal_env
 
+echo "Beginning script $1"
 # run script present as the first argument
-python $1
+python -u $1
