7304b90940e2c691747e5e1bdd74aaa05ff0b464
Andrew Reid Marderstein <amarder@smsh11dsu-srcf-d15-36.scg.stanford.edu>
2022-04-28 20:59:32 -0700

    update

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -2,7 +2,7 @@
 
 # example run:
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=256G --nodes=1 --ntasks=1 --cpus-per-task=1 run_script.sh subset_data.py
-# sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 run_script.sh plots_v3.py
+# sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 run_script.sh plots_v4.py
 
 echo "Start"
 source /oak/stanford/groups/smontgom/amarder/bin/conda_init.sh 
