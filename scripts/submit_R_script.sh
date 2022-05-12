ea14f8a187756e6f07397177ef5d6f420bc9d08b
Andrew Reid Marderstein <amarder@smsh11dsu-srcf-d15-35.scg.stanford.edu>
2022-05-12 16:24:56 -0700

    update

--- scripts/submit_R_script.sh
+++ scripts/submit_R_script.sh
@@ -2,7 +2,7 @@
 
 # example run:
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh anndata_to_seurat.R
-# sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh pseudobulk_de.R
+# sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh pseudobulk_de_v3.R
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh pseudobulk_de_test.R
 
 module load R/4.0
