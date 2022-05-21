c98926b42ae35d28e912ff72f4ada04876cedce9
drewmard <arm286@cornell.edu>
2022-05-20 21:30:07 -0400

    update

--- scripts/pseudobulk_de_v3_partC.R
+++ scripts/pseudobulk_de_v3_partC.R
@@ -6,44 +6,37 @@ library('BiocParallel')
 # sampletype="Liver"
 subset_column="sample"
 
-for (sampletype in c("Liver","Femur")) {
+for (disease_status in c("Healthy","DownSyndrome")) {
   
   print(sampletype)
   
   # 
   print("Reading metadata1...")
-  disease_status="Healthy"
+  sample_type="Liver"
   f = paste0("/oak/stanford/groups/smontgom/amarder/t21-proj/out/full/cellComp/10X_",disease_status,"_",sampletype,".cellComp.csv")
-  meta1.full<-fread(f,data.table = F,stringsAsFactors = F)
-  cells1 <- unique(meta1.full[,6])
-  meta1 <- unique(meta1.full[,c("patient","sample","sorting")])
-  meta1$environment <- disease_status
+  meta1<-fread(f,data.table = F,stringsAsFactors = F)
+  cells1 <- unique(meta1[,6])
+  meta1 <- unique(meta1[,c("patient","sample")])
+  meta1$environment <- sample_type
   
   # 
   print("Reading metadata2...")
-  disease_status="DownSyndrome"
+  sample_type="Femur"
   f = paste0("/oak/stanford/groups/smontgom/amarder/t21-proj/out/full/cellComp/10X_",disease_status,"_",sampletype,".cellComp.csv")
-  meta2.full<-fread(f,data.table = F,stringsAsFactors = F)
-  cells2 <- unique(meta2.full[,6])
-  meta2 <- unique(meta2.full[,c("patient","sample","sorting")])
-  meta2$environment <- disease_status
-  
-  colnames(meta1.full)[6] <- "leiden_names"
-  colnames(meta2.full)[6] <- "leiden_names"
-  meta.all.full <- rbind(meta1.full,meta2.full)
+  meta2<-fread(f,data.table = F,stringsAsFactors = F)
+  cells2 <- unique(meta2[,6])
+  meta2 <- unique(meta2[,c("patient","sample")])
+  meta2$environment <- sample_type
   
   #
   print("Merge metadata...")
   x <- rbind(meta1,meta2)
   rownames(x) <- x[,subset_column]
-  x$sorting[!(x$sorting %in% c("CD235a-","CD45+"))] <- "Other"
-  
   
   print("cell types of interest...")
   clusters_for_DE <- cells1[cells1 %in% cells2]
   P <- length(clusters_for_DE)
   
-  cell_type=clusters_for_DE[2]
   iter=0; for (cell_type in clusters_for_DE) {
     iter = iter + 1
     print(paste0(iter,"/",P,": ",cell_type))
@@ -53,21 +46,10 @@ for (sampletype in c("Liver","Femur")) {
     df.aggre <- fread(f,data.table = F,stringsAsFactors = F)
     rownames(df.aggre) <- df.aggre[,1]; df.aggre <- df.aggre[,-1]
     
-    metadata_to_use <- x[rownames(x) %in% colnames(df.aggre),]
-    to_merge <- names(table(metadata_to_use$sorting)[table(metadata_to_use$sorting) < 5])
-    df.aggre <- as.matrix(df.aggre[,match(rownames(metadata_to_use),colnames(df.aggre))])
-    
-    tab <- table(meta.all.full[meta.all.full[,"leiden_names"]==cell_type,'sample'])
-    samples_to_keep <- names(tab)[tab >= 10]
-    samples_to_keep <- samples_to_keep[samples_to_keep %in% rownames(metadata_to_use)]
-    df.aggre <- df.aggre[,samples_to_keep]
-    metadata_to_use <- metadata_to_use[samples_to_keep,]
+    df.aggre <- as.matrix(df.aggre[,match(rownames(x),colnames(df.aggre))])
     
     # Standard usage of limma/voom
     geneExpr = DGEList( df.aggre )
-    keep <- filterByExpr(geneExpr, group=metadata_to_use$environment)
-    geneExpr <- geneExpr[keep,]
-    
     geneExpr = calcNormFactors( geneExpr )
     
     # Specify parallel processing parameters
@@ -75,28 +57,25 @@ for (sampletype in c("Liver","Femur")) {
     param = SnowParam(8, "SOCK", progressbar=TRUE)
     
     # The variable to be tested must be a fixed effect
-    # form <- ~ environment + (1|patient) 
-    form <- ~ environment + patient + sorting
+    form <- ~ environment + (1|patient) 
     
     # estimate weights using linear mixed model of dream
-    vobjDream = voomWithDreamWeights( geneExpr, form, metadata_to_use, BPPARAM=param )
+    vobjDream = voomWithDreamWeights( geneExpr, form, x, BPPARAM=param )
     
     # A positive FC is increased expression in the DS compared to healthy
-    metadata_to_use$environment <- factor(metadata_to_use$environment,levels=c("Healthy","DownSyndrome"))
+    x$environment <- factor(x$environment,levels=c("Femur","Liver"))
     
     # Fit the dream model on each gene
     # By default, uses the Satterthwaite approximation for the hypothesis test
-    fitmm = dream( vobjDream, form, metadata_to_use )
-    fitmm = eBayes(fitmm)
+    fitmm = dream( vobjDream, form, x )
     
     # reorganize:
-    # res.df <- as.data.frame(topTable( fitmm, number=Inf))
-    res.df <- as.data.frame(topTable( fitmm, number=Inf,coef = "environmentDownSyndrome" ))
+    res.df <- as.data.frame(topTable( fitmm, number=Inf ))
     res.df$names <- rownames(res.df)
     
     # save:
     system("mkdir -p /oak/stanford/groups/smontgom/amarder/t21-proj/out/full/DE_pb_leiden_names")
-    f.out <- paste0("/oak/stanford/groups/smontgom/amarder/t21-proj/out/full/DE_pb_leiden_names/",sampletype,".",cell_type_filename,".",subset_column,".txt")
+    f.out <- paste0("/oak/stanford/groups/smontgom/amarder/t21-proj/out/full/DE_pb_leiden_names/",disease_status,".",cell_type_filename,".",subset_column,".txt")
     fwrite(res.df,f.out,quote = F,na = "NA",sep = '\t',row.names = F,col.names = T)
     
   }
