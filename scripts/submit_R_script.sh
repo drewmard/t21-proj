9a48997e32dc8d6de4255108606f4745eb0cf7f0
Andrew Reid Marderstein <amarder@smsh11dsu-srcf-d15-36.scg.stanford.edu>
2022-07-10 14:52:39 -0700

    update

--- scripts/submit_R_script.sh
+++ scripts/submit_R_script.sh
@@ -5,6 +5,8 @@
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh pseudobulk_de_v3.R
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh pseudobulk_de_test.R
 # srun --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 --pty bash
+# sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=256G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh create_cell_data.R
+# sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh create_meta_files.R
 
 # sbatch --account=smontgom --partition=batch --time=24:00:00 --mem=356G --nodes=1 --ntasks=1 --cpus-per-task=1 submit_R_script.sh downsample_de.R
 
