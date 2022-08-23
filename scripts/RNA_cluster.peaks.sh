8513783affa25579478b3b3318cf979f1ce9b045
drewmard <arm286@cornell.edu>
2022-08-23 09:09:37 -0700

    update

--- scripts/RNA_cluster.peaks.sh
+++ scripts/RNA_cluster.peaks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# # # sbatch --account=smontgom --partition=batch --time=7-1:00:00 --mem=128G --nodes=1 --ntasks=1 /oak/stanford/groups/smontgom/amarder/t21-proj/RNA_cluster.peaks.sh DS_Multiome_h
+# # # sbatch --account=smontgom --partition=batch --time=3-1:00:00 --mem=128G --nodes=1 --ntasks=1 /oak/stanford/groups/smontgom/amarder/t21-proj/scripts/RNA_cluster.peaks.sh DS_Multiome_h
 
-Rscript /oak/stanford/groups/smontgom/amarder/t21-proj/scripts/RNA_cluster.peaks.R $dataset
+Rscript /oak/stanford/groups/smontgom/amarder/t21-proj/scripts/RNA_cluster.peaks.R $1
