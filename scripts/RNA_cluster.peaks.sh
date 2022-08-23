1c9449253b4241ba7410adce9d9df680813fb987
drewmard <arm286@cornell.edu>
2022-08-23 10:07:13 -0700

    update

--- scripts/RNA_cluster.peaks.sh
+++ scripts/RNA_cluster.peaks.sh
@@ -3,7 +3,7 @@
 # # # sbatch --account=smontgom --partition=batch --time=3-1:00:00 --mem=128G --nodes=1 --ntasks=1 /oak/stanford/groups/smontgom/amarder/t21-proj/scripts/RNA_cluster.peaks.sh DS_Multiome_h 0 3
 
 module load R/4.1.2
-dir="/oak/stanford/groups/smontgom/amarder/neuro-variants"
+dir=/oak/stanford/groups/smontgom/amarder/neuro-variants
 dataset=$1
 start=$2
 end=$3
